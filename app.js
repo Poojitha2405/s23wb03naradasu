b8593a675972cd144a21908ee98173056d062c18
Poojitha2405 <npoojitha2405@gmail.com>
2023-04-05 10:34:57 -0500

    Added images

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var customerRouter = require('./routes/Customer');
+var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/Selector');
+
 
 var app = express();
 
@@ -21,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Customer', customerRouter);
+app.use('/board', boardRouter);
+app.use('/Selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
